a2a60e5a4e699983e5bb35d32bd2d5fa34182724
Mikita Buinavets <mikita.buinavets@arua.co>
2025-06-26 21:59:36 +0300

    Implement functional interface methods in `InterfaceCreator`

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -9,24 +10,36 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class InterfaceCreator {
-
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> x.stream().allMatch(s -> Character.isUpperCase(s.charAt(0)));
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> x.addAll(x.stream()
+                .filter(num -> num % 2 == 0)
+                .collect(java.util.stream.Collectors.toList()));
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return () -> values.stream()
+                .filter(s -> Character.isUpperCase(s.charAt(0)))
+                .filter(s -> s.endsWith("."))
+                .filter(s -> s.split("\\s+").length > 3)
+                .collect(java.util.stream.Collectors.toList());
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> x.stream().collect(
+                java.util.stream.Collectors.toMap(
+                        s -> s,
+                        String::length));
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list1, list2) -> {
+            ArrayList<Integer> result = new ArrayList<>(list1);
+            result.addAll(list2);
+            return result;
+        };
     }
 }
